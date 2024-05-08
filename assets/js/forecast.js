eb5383c1d8d573d9dc00328ac8e205737159609a
Ted Perez <taperez1989@yahoo.com>
2024-05-07 21:23:20 -0700

    attempts at appending current forcast

--- assets/js/forecast.js
+++ assets/js/forecast.js
@@ -2,13 +2,14 @@ const searchFormEl = document.querySelector('.form');
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
-
+// grabs the value input by the user in the form ex. user enters "san diego"
     const searchInputVal = document.getElementById('city-search').value;
 
     if (!searchInputVal) {
         console.error('search input value required');
         return;
     }
+    // the value of searchInputVal is then entered in this url for this lat lon variable in order for the first part of the fetch request to run.
     const latLon = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputVal}&limit=1&appid=f8ad66aaefdb1e05f8e8a305f8140066`;
     // fetch request for lat and lon data
     fetch(latLon)
@@ -20,12 +21,12 @@ function handleSearchFormSubmit(event) {
 
             return response.json();
         })
-        // fetch lat and lon
+        // fetch lat and lon 
         .then(function (data) {
             console.log(data);
             const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=f8ad66aaefdb1e05f8e8a305f8140066`;
 
-            return fetch(apiUrl)
+            return fetch(apiUrl);
 
         })
         // if response is false return erro
@@ -61,6 +62,23 @@ function handleSearchFormSubmit(event) {
             }
 
         })
+        // .then(function (data) {
+        //     const humidWind = `https://api.openweathermap.org/data/2.5/forecast?humidity=${data.list[0].main.humidity}&windSpeed=${data.list[0].wind.speed}&appid=f8ad66aaefdb1e05f8e8a305f8140066`;
+        //     console.log(data)
+        //     return fetch(humidWind);
+        
+        // })
+
+        // .then(function (data) {
+        //     const humidity = data.list.main.humidity;
+        //     const windSpeed = data.list[0].wind.speed;
+
+        //     console.log('humidity', humidity);
+        //     console.log('wind wpeed', windSpeed);
+
+        //     return fetch(humidity, windSpeed);
+            
+        // })
 
         // an error at any step of the .thens it stops function
         .catch(function (err) {
@@ -80,14 +98,39 @@ function searchHistory() {
 // add in add class for the css on the search history cities
         searchedCity.textContent = cities;
         historyContainer.append(searchedCity);
-        console.log(cities);
+        // console.log(cities);
     });
 
     
 }
 searchHistory();
 
-    
+
+function currentForecast() {
+    const resultContainer = document.querySelector('.current-result');
+
+    const currentHumidity = document.createElement('p');
+    const currentWind = document.createElement('p');
+
+    const currentTemp = document.createElement('p');
+    const titleEl = document.createElement('h3');
+
+    currentHumidity.textContent = humidity;
+    currentWind.textContent = wind;
+    currentTemp.textContent = temp; 
+    titleEl.textContent = cityName;
+
+
+    resultContainer.append(currentHumidity);
+    resultContainer.append(currentWind);
+    resultContainer.append(currentTemp);
+    resultContainer.append(titleEl);
+
+
+}
+currentForecast()
+
+// const cityName = data.city.name;
 
 
 // dynamically create elements to append information to containers
@@ -121,3 +164,4 @@ searchHistory();
 //         bodyContentEl.innerHTML +=
 //             '<strong>Subjects:</strong> No subject for this entry.';
 //     }
+
